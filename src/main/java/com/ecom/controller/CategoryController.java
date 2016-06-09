04989a0e532f1e79d70c4cbdf0b1993e8ec18b7d
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2016-06-09 12:05:24 +0300

    Adding offers on category cortroller

--- src/main/java/com/ecom/controller/CategoryController.java
+++ src/main/java/com/ecom/controller/CategoryController.java
@@ -1,7 +1,15 @@
 package com.ecom.controller;
 
-/**
- * Created by Workdev on 09/06/2016.
- */
+import com.ecom.model.Category;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoryController {
+
+    @RequestMapping("/category/{categoryId}/{categoryValue}")
+    public Category getCategory(@PathVariable String categoryId, @PathVariable String categoryValue) {
+        return new Category();
+    }
 }
