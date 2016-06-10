1ae200524f676a1fb903e55565112da5da058d0f
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2016-06-10 11:34:05 +0300

    Add Adress and Vendor model

---
+++ src/main/java/com/ecom/model/Adress.java
@@ -0,0 +1,8 @@
+package com.ecom.model;
+
+public class Adress {
+    private String number;
+    private String roadName;
+    private String country;
+    private String state;
+}
---
+++ src/main/java/com/ecom/model/Vendor.java
@@ -0,0 +1,7 @@
+package com.ecom.model;
+
+public class Vendor {
+    private String vendorId;
+    private String vendorName;
+    private Adress adress;
+}
