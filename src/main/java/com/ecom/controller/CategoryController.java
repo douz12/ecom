d653b375f56cc6c81f33548caf1dac28c64a1035
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2016-06-13 14:53:07 +0300

    update categoryController

--- src/main/java/com/ecom/controller/CategoryController.java
+++ src/main/java/com/ecom/controller/CategoryController.java
@@ -1,12 +1,11 @@
 package com.ecom.controller;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ProtocolVersion;
-import com.datastax.driver.mapping.Mapper;
-import com.datastax.driver.mapping.MappingManager;
-import com.ecom.model.Address;
+import com.ecom.accessor.CategoryAccessor;
+import com.ecom.common.config.CassandraConfig;
 import com.ecom.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     @Autowired
-    private MappingManager mappingManager;
+    private CassandraConfig cassandraConfig;
 
     @RequestMapping("/category/{categoryId}/{categoryValue}")
-    public Category getCategory(@PathVariable String categoryId, @PathVariable String categoryValue) {
-        Mapper<Address> addressMapper = mappingManager.mapper(Address.class);
-        Address address = new Address();
-        address.setAddressId("B55");
-        address.setCountry("Antananarivo");
-        addressMapper.save(address);
-        return new Category();
+    public ResponseEntity<Category> getCategory(@PathVariable String categoryId, @PathVariable String categoryValue) {
+        Category category = cassandraConfig
+                .getMappingManager()
+                .createAccessor(CategoryAccessor.class)
+                .getCategoryById(categoryId, categoryValue)
+                .one();
+        if (category == null) {
+            System.err.println("Category id [" + categoryId + "] not found.");
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Category>(category, HttpStatus.OK);
     }
 }
 
