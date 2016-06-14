0815fc21018520a7b2b3a73de40a8497daafe162
douz12 <naris.razafimahatratra@zoho.com>
2016-06-14 22:10:52 +0300

    base couchdb project

--- src/main/java/com/ecom/controller/CategoryController.java
+++ src/main/java/com/ecom/controller/CategoryController.java
@@ -1,8 +1,5 @@
 package com.ecom.controller;
 
-import com.ecom.accessor.CategoryAccessor;
-import com.ecom.common.config.CassandraConfig;
-import com.ecom.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +8,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class CategoryController {
 
-    @Autowired
-    private CassandraConfig cassandraConfig;
-
     @RequestMapping("/category/{categoryId}/{categoryValue}")
-    public ResponseEntity<Category> getCategory(@PathVariable String categoryId) {
-        Category category = cassandraConfig
-                .getMappingManager()
-                .createAccessor(CategoryAccessor.class)
-                .getCategoryById(categoryId)
-                .one();
-        if (category == null) {
-            System.err.println("Category id [" + categoryId + "] not found.");
-            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<Category>(category, HttpStatus.OK);
-    }
+    public void getCategory(@PathVariable String categoryId) {
 
-    @RequestMapping(value = "/category/add", method = RequestMethod.POST)
-    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
-        cassandraConfig.getMappingManager().mapper(Category.class).save(category);
-        return  new ResponseEntity<Category>(category, HttpStatus.OK);
     }
 }
 
